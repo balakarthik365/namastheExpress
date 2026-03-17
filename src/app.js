260beaefe198713ae0d9bb94b6fe1ecd2262f723
balakarthik <balakarthik364@gmail.com>
2026-03-17 23:28:11 +0530

    HTTP calls

--- src/app.js
+++ src/app.js
@@ -1,14 +1,14 @@
 const express = require("express");
 const app = express();
 
-app.get("/test", (req, res) => {
-  res.send("page1");
+app.get("/user", (req, res) => {
+  res.send({ firstName: "Balakarthik", lastName: "Pendyala" });
 });
-app.get("/hello", (req, res) => {
-  res.send("page2");
+app.post("/user", (req, res) => {
+  res.send("Data saved to data base");
 });
-app.get("/", (req, res) => {
-  res.send("Hello world this is node js");
+app.delete("/user", (req, res) => {
+  res.send("Data deleted from data base");
 });
 app.listen(3000, () => {
   console.log("server is running on port 3000");
