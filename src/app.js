0886e12cb862762c899c66aa62ae1f1b292720ef
balakarthik <balakarthik364@gmail.com>
2026-03-25 22:32:57 +0530

    add data dynamically using post

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const connectDB = require("./config/database");
 const app = express();
 const User = require("./models/user");
+app.use(express.json());
 /*const {
   checkUserAuthorization,
   checkAdminAuthorization,
@@ -40,15 +41,7 @@ app.delete("/user/deleteUser", (req, res) => {
 });*/
 //create the route handler
 app.post("/signup", async (req, res) => {
-  const user = new User({
-    firstName: "Kalyani",
-    lastName: "Dhikonda",
-    email: "kalyani.dhikonda@gmail.com",
-    password: "test123",
-    age: 30,
-    gender: "female",
-    phone: 8374755222,
-  });
+  const user = new User(req.body);
   try {
     await user.save();
     res.send("User created successfully");
